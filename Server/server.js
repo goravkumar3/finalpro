b7a48329f862f9ca6411cce716694a1bc62d5019
kashishkumari6677 <aneelkashish@gmail.com>
2024-05-14 17:53:53 +0500

    update

--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,7 @@
 const express=require('express')
+const routers=require('./router/storerouter')
 const app=express()
-
-
-
+app.use("/",routers);
 app.listen(5000,()=>{
 console.log("port listening")
 })
